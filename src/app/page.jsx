5fd0911de8ae4810e2c296ee28ac51def0a41a6b
Carlos Aviles Nuñez <122574326+cavilesnunez@users.noreply.github.com>
2023-09-28 17:28:39 -0600

    First

---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+        debitis beatae mollitia, incidunt accusamus laborum autem quaerat. Nisi,
+        eius exercitationem quas non, maiores quam eum accusantium aperiam est
+        velit iusto.
+      </p>
+    </div>
+  );
+}
